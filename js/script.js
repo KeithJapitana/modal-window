cc3adefe90f8d5dd3649b4fa998bf3b24e588ebe
keith japitana <keithjapitana1996@gmail.com>
2024-03-16 10:25:51 +0800

    defining the elemnts.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosemodal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++){
+    console.log(btnOpenModal[i].textContent);
+
+}
